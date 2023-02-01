e2553e7b4d1a4657a00f0c083147625ad60cf174
badgersky <szymonek.ptasznik@gmail.com>
2023-02-01 13:26:19 +0100

    users table creation script

--- create_db.py
+++ create_db.py
@@ -4,16 +4,28 @@ from psycopg2 import connect, OperationalError, DatabaseError
 HOST = 'localhost'
 USER = 'postgres'
 PASSWORD = 'coderslab'
+DB = 'users'
 
+
+# database creation
+cnx = connect(host=HOST, user=USER, password=PASSWORD)
+cnx.autocommit = True
+sql = """CREATE DATABASE users;"""
 try:
-    cnx = connect(host=HOST, user=USER, password=PASSWORD)
-    cnx.autocommit = True
-    sql = """CREATE DATABASE users;"""
     with cnx.cursor() as cursor:
         cursor.execute(sql)
 except (OperationalError, DatabaseError) as err:
-    if 'already exists' not in str(err):
-        print('Unable to connect')
     print('database already exists')
+cnx.close()
 
 
+# table users creation
+cnx = connect(host=HOST, user=USER, password=PASSWORD, database=DB)
+cnx.autocommit = True
+users_sql = """CREATE TABLE users (id serial, username varchar(255), hashed_passw varchar(80), PRIMARY KEY (id));"""
+try:
+    with cnx.cursor() as cursor:
+        cursor.execute(users_sql)
+except (OperationalError, DatabaseError) as err:
+    if 'already exists' in str(err):
+        print('table already exists')

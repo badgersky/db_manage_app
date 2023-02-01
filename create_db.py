5293e34698435e55ad4da5d920a75448269095b1
badgersky <szymonek.ptasznik@gmail.com>
2023-02-01 13:55:24 +0100

    unique atribute, cascade deleting

--- create_db.py
+++ create_db.py
@@ -28,7 +28,8 @@ cnx.close()
 # table users creation
 cnx = connect(host=HOST, user=USER, password=PASSWORD, database=DB)
 cnx.autocommit = True
-users_sql = """CREATE TABLE users (id serial, username varchar(255), hashed_passw varchar(80), PRIMARY KEY (id));"""
+users_sql = """CREATE TABLE users (id serial, username varchar(255) UNIQUE,
+hashed_passw varchar(80), PRIMARY KEY (id));"""
 try:
     with cnx.cursor() as cursor:
         cursor.execute(users_sql)
@@ -38,7 +39,7 @@ except (OperationalError, DatabaseError) as err:
 # table messages creation
 messages_sql = """CREATE TABLE messages (id serial, from_id int NOT NULL UNIQUE , to_id int NOT NULL UNIQUE,
 message_date TIMESTAMP NOT NULL DEFAULT CURRENT_DATE, text varchar(255), PRIMARY KEY (id),
-FOREIGN KEY (from_id) REFERENCES users(id), FOREIGN KEY (TO_ID) REFERENCES users(id));"""
+FOREIGN KEY (from_id) REFERENCES users(id), FOREIGN KEY (TO_ID) REFERENCES users(id) ON DELETE CASCADE);"""
 try:
     with cnx.cursor() as cursor:
         cursor.execute(messages_sql)

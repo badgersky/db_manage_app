568824b6372d951350525cdc1a7d0fcdf08718d5
badgersky <szymonek.ptasznik@gmail.com>
2023-02-04 19:45:40 +0100

    sending messages functionality

--- messages.py
+++ messages.py
@@ -23,6 +23,26 @@ def list_messages(cursor, username, password):
         messages = Message.load_all_messages(cursor)
         for message in messages:
             if message.to_id == user.id:
-                print(message.date, ': ', message.mess)
+                print(f'addressee: {user.username}')
+                print(f'date: {message.date}')
+                print(f'text: {message.mess}')
     else:
         print('Password incorrect')
+
+
+def send_message(cursor, username, password, addressee, message):
+    user = User.load_by_username(cursor, username)
+    if not user:
+        print('User does not exist')
+    elif bcrypt.checkpw(bytes(password, encoding='utf-8'), user.hashed_password):
+        user_to = User.load_by_username(cursor, addressee)
+        if not user_to:
+            print('Pointed addressee does not exist')
+        else:
+            if len(message) > 255:
+                print('Message is to long')
+            else:
+                m = Message(user.id, user_to.id, message)
+                m.save_to_db(cursor)
+    else:
+        print('Password is incorrect')

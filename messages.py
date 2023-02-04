cc1881824315611d815f64bdd875129d735960e8
badgersky <szymonek.ptasznik@gmail.com>
2023-02-04 19:52:49 +0100

    app logic added

--- messages.py
+++ messages.py
@@ -46,3 +46,26 @@ def send_message(cursor, username, password, addressee, message):
                 m.save_to_db(cursor)
     else:
         print('Password is incorrect')
+
+
+if __name__ == '__main__':
+    HOST = 'localhost'
+    USER = 'postgres'
+    PASSWORD = 'coderslab'
+    DB = 'users_db'
+
+    try:
+        cnx = connect(host=HOST, user=USER, password=PASSWORD, database=DB)
+        cnx.autocommit = True
+        c = cnx.cursor()
+
+        if args.username and args.password and args.list:
+            list_messages(c, args.username, args.password)
+        elif args.username and args.password and args.to and args.send:
+            send_message(c, args.username, args.password, args.to, args.send)
+
+        c.close()
+        cnx.close()
+
+    except OperationalError as e:
+        print('error: ', e)
